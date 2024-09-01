bbdc14574de499fecb72b01ed3288cbb11d7f168
Washington <was.antonio.xy@gmail.com>
2024-09-01 19:30:08 -0400

    feat: header, title and styling

--- to-do-app/App.js
+++ to-do-app/App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Logo from './assets/rocket.png'; // Importe o arquivo PNG
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>To-do App start (:</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Image style={styles.logo} source={Logo} />
+          <Text style={styles.appTitleBlue}>to</Text>
+          <Text style={styles.appTitlePurple}>do</Text>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#262626',
   },
+  appTitleBlue: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#4EA8DE',
+  },
+  appTitlePurple: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#5E60CE',
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: '100%',
+    height: 250,
+
+    backgroundColor: '#0D0D0D'
+  },
+  logo: {
+    resizeMode: 'contain',
+    
+    marginRight: 10
+  }
 });
