62c12e533aa1c94e6c6eef9238251e74b1e86182
Washington <was.antonio.xy@gmail.com>
2024-09-01 19:39:26 -0400

    refactor: organizing header in components

--- to-do-app/App.js
+++ to-do-app/App.js
@@ -1,52 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import Logo from './assets/rocket.png'; // Importe o arquivo PNG
+import Header from './components/header';
 
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Image style={styles.logo} source={Logo} />
-          <Text style={styles.appTitleBlue}>to</Text>
-          <Text style={styles.appTitlePurple}>do</Text>
-        </View>
-      </View>
+      <Header />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#262626',
-  },
-  appTitleBlue: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#4EA8DE',
-  },
-  appTitlePurple: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#5E60CE',
-  },
-  headerContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
 
-    width: '100%',
-    height: 250,
-
-    backgroundColor: '#0D0D0D'
-  },
-  logo: {
-    resizeMode: 'contain',
-    
-    marginRight: 10
-  }
-});
